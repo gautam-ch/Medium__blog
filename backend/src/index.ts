c98a4551e683837f5b71d2555cf0a0b822bfa5ab
gautam-ch <147068398+gautam-ch@users.noreply.github.com>
2025-01-17 18:49:21 +0530

    updating index.ts and wrangler.toml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
+import User from './routes/user'
+import Posts from './routes/post'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+
+app.route('/api/v1/user',User);
+
+app.route('/api/v1/post',Posts);
+
+
+
+
 
 export default app
