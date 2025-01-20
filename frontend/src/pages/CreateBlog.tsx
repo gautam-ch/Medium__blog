e66b6b95a9ae5fdd415a8c41923994fe1949aeae
gautam-ch <147068398+gautam-ch@users.noreply.github.com>
2025-01-20 14:40:15 +0530

    build dist

--- frontend/src/pages/CreateBlog.tsx
+++ frontend/src/pages/CreateBlog.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { AppBar } from "../components/AppBar";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
